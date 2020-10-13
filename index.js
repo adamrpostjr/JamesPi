c18645e42b185a99cb3beb725228d2ef50310685
Adam Post <adamrpostjr@gmail.com>
2020-10-13 16:35:30 -0400

    adding more plugins

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public/'));
+app.use(bodyParser.json());
