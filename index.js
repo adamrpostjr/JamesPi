72e45f0ba792ae885a273df64074410f22bcc18c
Adam Post <adamrpostjr@gmail.com>
2020-10-13 16:42:48 -0400

    adding ejs package and getting a server up and running 🤓

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ app.set('view-engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public/'));
 app.use(bodyParser.json());
+
+app.get('/', (req, res, next)=>{
+  res.render(__dirname + '/public/index.ejs')
+})
+
+server.listen(8085, ()=>{console.log('listening on 8085');})
+
